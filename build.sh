ff4e99d12f773cb9534287d585b9a4d6bebd05b7
Thomas Jepp <tom@tomjepp.co.uk>
2021-03-15 23:34:29 +0000

    update build script

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e 
+
+echo "Fetching..."
+git remote update
+
+echo "Rebasing..."
+git checkout medialib-tj
+git branch -f medialib upstream/medialib
+git rebase -p medialib
+git submodule update --init --recursive
+
+if [ -d osx/build ]; then
+	echo "Cleaning up..."
+	rm -Rf osx/build
+fi
+
+echo "Building..."
+xcodebuild -project osx/deadbeef.xcodeproj -target DeaDBeeF -configuration Release
+
+echo "Copying to place..."
+if [ -d /Applications/DeaDBeef.old.app ]; then
+	sudo rm -Rf /Applications/DeaDBeeF.old.app
+fi
+
+if [ -d /Applications/DeaDBeef.app ]; then
+	sudo mv /Applications/DeaDBeeF.app /Applications/DeaDBeeF.old.app
+fi
+
+git push --force-with-lease origin medialib-tj
+
+sudo rsync --delete -a osx/build/Release/DeaDBeeF.app/ /Applications/DeaDBeeF.app
+echo "Done!"
